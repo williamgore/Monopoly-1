6ec0987ebdacd2c3c64484d15f8f588611e28c9e
Raden Pablo <radenpablo292@gmail.coml>
2019-05-23 13:59:26 -0500

    Monopoly Game

---
+++ src/monopoly/Monopoly.java
@@ -0,0 +1,17 @@
+
+package monopoly;
+
+/**
+ *
+ * @author Raden Pablo and Will Gore
+ */
+public class Monopoly {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
